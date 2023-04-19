b68b10b7f953bb785e06c243e036fda060e24a47
nebojsaklasanovic <nebojsa.shone9998@gmail.com>
2023-04-19 14:34:31 +0200

    Overhaul

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { SiCss3, SiReact, SiTailwindcss } from "react-icons/si";
+import { TbApi } from "react-icons/tb";
 import Carousel from "../assets/images/projects/carousel.png";
 import Fastfood from "../assets/images/projects/fastfood.png";
 import Todo from "../assets/images/projects/todo.png";
@@ -15,17 +17,32 @@ const Projects = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
           <div className="bg-gray-100 rounded-lg shadow-xl shadow-gray overflow-hidden hover:scale-105 duration-500">
             <img src={Weather} alt="/" className="w-full h-48 object-cover" />
-            <div className="p-6">
+            <div className="flex flex-col p-6 justify-center items-center">
               <h3 className="text-xl font-bold mb-2">Weather Forecast</h3>
-              <p className="hidden md:block text-gray-800 mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                nec quam urna. Donec malesuada est id quam sagittis, vitae
-                efficitur neque eleifend.
-              </p>
+              <ul className="hidden md:flex gap-6 mb-2 font-bold">
+                <li>
+                  <SiReact
+                    className="text-cyan-700 rounded-full"
+                    size={40}
+                  />
+                </li>
+                <li>
+                  <SiTailwindcss
+                    className="text-cyan-700 rounded-full"
+                    size={40}
+                  />
+                </li>
+                <li>
+                  <TbApi
+                    className="text-cyan-700 rounded-full"
+                    size={40}
+                  />
+                </li>
+              </ul>
               <a
                 href="https://weather-forecast-all.netlify.app/"
                 target="_blank"
-                className="inline-block bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white py-2 px-4 rounded-xl"
+                className="mt-2 inline-block bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white font-semibold py-2 px-4 rounded-xl"
               >
                 Show project
               </a>
@@ -33,16 +50,28 @@ const Projects = () => {
           </div>
           <div className="bg-gray-100 rounded-lg shadow-xl shadow-gray overflow-hidden hover:scale-105 duration-500">
             <img src={Fastfood} alt="/" className="w-full h-48 object-cover" />
-            <div className="p-6">
-              <h3 className="text-xl font-bold mb-2">Dash n' Dine</h3>
-              <p className="hidden md:block text-gray-800 mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                nec quam urna. Donec malesuada est id quam sagittis, vitae
-                efficitur neque eleifend.
-              </p>
+            <div className="flex flex-col p-6 justify-center items-center">
+              <h3 className="text-xl font-bold mb-2">
+                Dash n' Dine - Landing Page
+              </h3>
+              <ul className="hidden md:flex gap-6 mb-2 font-bold">
+                <li>
+                  <SiReact
+                    className="text-cyan-700 rounded-full"
+                    size={40}
+                  />
+                </li>
+                <li>
+                  <SiTailwindcss
+                    className="text-cyan-700 rounded-full"
+                    size={40}
+                  />
+                </li>
+              </ul>
               <a
-                href="#"
-                className="inline-block bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white py-2 px-4 rounded-xl"
+                href="https://dashndine-project.netlify.app"
+                target="_blank"
+                className="mt-2 inline-block bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white font-semibold py-2 px-4 rounded-xl"
               >
                 Show project
               </a>
@@ -50,17 +79,26 @@ const Projects = () => {
           </div>
           <div className="bg-gray-100 rounded-lg shadow-xl shadow-gray overflow-hidden hover:scale-105 duration-500">
             <img src={Todo} alt="/" className="w-full h-48 object-cover" />
-            <div className="p-6">
+            <div className="flex flex-col p-6 justify-center items-center">
               <h3 className="text-xl font-bold mb-2">Todo App</h3>
-              <p className="hidden md:block text-gray-800 mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                nec quam urna. Donec malesuada est id quam sagittis, vitae
-                efficitur neque eleifend.
-              </p>
+              <ul className="hidden md:flex gap-6 mb-2 font-bold">
+                <li>
+                  <SiReact className="text-cyan-700 rounded-full" size={40} />
+                </li>
+                <li>
+                  <SiTailwindcss
+                    className="text-cyan-700 rounded-full"
+                    size={40}
+                  />
+                </li>
+                <li>
+                  <SiCss3 className="text-cyan-700 rounded-full" size={40} />
+                </li>
+              </ul>
               <a
                 href="https://todolistprojecttasks.netlify.app"
                 target="_blank"
-                className="inline-block bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white py-2 px-4 rounded-xl"
+                className="mt-2 inline-block bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white font-semibold py-2 px-4 rounded-xl"
               >
                 Show project
               </a>
@@ -68,17 +106,26 @@ const Projects = () => {
           </div>
           <div className="bg-gray-100 rounded-lg shadow-xl shadow-gray overflow-hidden hover:scale-105 duration-500">
             <img src={Carousel} alt="/" className="w-full h-48 object-cover" />
-            <div className="p-6">
+            <div className="flex flex-col p-6 justify-center items-center">
               <h3 className="text-xl font-bold mb-2">Image Carousel</h3>
-              <p className="hidden md:block text-gray-800 mb-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla
-                nec quam urna. Donec malesuada est id quam sagittis, vitae
-                efficitur neque eleifend.
-              </p>
+              <ul className="hidden md:flex gap-6 mb-2 font-bold">
+                <li>
+                  <SiReact className="text-cyan-700 rounded-full" size={40} />
+                </li>
+                <li>
+                  <SiTailwindcss
+                    className="text-cyan-700 rounded-full"
+                    size={40}
+                  />
+                </li>
+                <li>
+                  <SiCss3 className="text-cyan-700 rounded-full" size={40} />
+                </li>
+              </ul>
               <a
                 href="https://imagecarouseproject.netlify.app"
                 target="_blank"
-                className="inline-block bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white py-2 px-4 rounded-xl"
+                className="mt-2 inline-block bg-gradient-to-r from-cyan-500 to-teal-500 bg-cyan-500 text-white font-semibold py-2 px-4 rounded-xl"
               >
                 Show project
               </a>
